eb324f3bba0dc859fde081eb088e4e4c7992c5d0
Bernard Bussy <brbitzbussy@gmail.com>
2018-07-09 10:21:00 +0200

    created main()

--- main.py
+++ main.py
@@ -34,51 +34,57 @@ def end_read(signal,frame):
     run = False
     rdr.cleanup()
     sys.exit()
-    
-signal.signal(signal.SIGINT, end_read)
-
-while run:
-    rdr.wait_for_tag()
-
-    (error, data) = rdr.request()
-    if not error:
-        print("\nDetected: " + format(data, "02x"))
-
-    (error, uid) = rdr.anticoll()
-    if not error:
-        print("Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3]))
 
 
 
-        id_str = str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])
+main()
+    signal.signal(signal.SIGINT, end_read)
 
-        data = {
-            "jsonrpc":"2.0",
-            "id":str(uuid.uuid4()),
-            "method":"TagEvent.Create",
-            "params":[{
-                "tag_event":{
-                    "tag_id":id_str,
-                    "tag_time": int(time.time())
-                }
-            }]
-        }
+    while run:
+        rdr.wait_for_tag()
+
+        (error, data) = rdr.request()
+        if not error:
+            print("\nDetected: " + format(data, "02x"))
+
+        (error, uid) = rdr.anticoll()
+        if not error:
+            print("Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3]))
 
 
-        try:
-            response = requests.post("http://192.168.8.103:9004/api", json=data, headers={"content-type":"application/json"})
-            print(str(response.content))
-        except requests.exceptions.RequestException as e:  #
-            print(e)
 
-       # print("Setting tag")
-       # util.set_tag(uid)
-       # print("\nAuthorizing")
-       # #util.auth(rdr.auth_a, [0x12, 0x34, 0x56, 0x78, 0x96, 0x92])
-       # util.auth(rdr.auth_b, [0x74, 0x00, 0x52, 0x35, 0x00, 0xFF])
-       # print("\nReading")
-       # util.read_out(4)
-       # print("\nDeauthorizing")
-       # util.deauth()
+            id_str = str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])
+
+            data = {
+                "jsonrpc":"2.0",
+                "id":str(uuid.uuid4()),
+                "method":"TagEvent.Create",
+                "params":[{
+                    "tag_event":{
+                        "tag_id":id_str,
+                        "tag_time": int(time.time())
+                    }
+                }]
+            }
+
+
+            try:
+                response = requests.post("http://192.168.8.103:9004/api", json=data, headers={"content-type":"application/json"})
+                print(str(response.content))
+            except requests.exceptions.RequestException as e:  #
+                print(e)
+
+           # print("Setting tag")
+           # util.set_tag(uid)
+           # print("\nAuthorizing")
+           # #util.auth(rdr.auth_a, [0x12, 0x34, 0x56, 0x78, 0x96, 0x92])
+           # util.auth(rdr.auth_b, [0x74, 0x00, 0x52, 0x35, 0x00, 0xFF])
+           # print("\nReading")
+           # util.read_out(4)
+           # print("\nDeauthorizing")
+           # util.deauth()
+        time.sleep(1)
     time.sleep(1)
-time.sleep(1)
+
+if __name__ == "__main__":
+    print("hello world. I am main!")
