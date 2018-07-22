5b31ff11edb0e3908c436ef5af2f1c7753f2ce15
imran <parukboy@gmail.com>
2018-07-22 20:41:33 +0200

    .

--- tools/webSocket/webSocket.py
+++ tools/webSocket/webSocket.py
@@ -32,6 +32,7 @@ class webSocket(threading.Thread):
     
     def __init__(self, url):
         self.url = url
+        self.ws = None
         threading.Thread.__init__(self)
 
     def run(self):
