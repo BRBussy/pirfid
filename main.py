fbbcd78cba7c5384f4d6f428126cfd43e9f5729c
imran <parukboy@gmail.com>
2018-07-29 17:51:57 +0200

    .

--- main.py
+++ main.py
@@ -84,9 +84,8 @@ class timekeeper(Thread):
     def run(self):
         while True:
             if (self.queue.qsize() > 0):
-                print("Queue Item: {0}".format(self.queue.get()))
-                # for item in iter(self.queue.get, None): # Replace `None` as you need.
-                #     print("Queue Item: {0}".format(item))
+                for item in iter(self.queue.get, None): # Replace `None` as you need.
+                    print("Queue Item: {0}".format(item))
             self.wait_for_tag_event()
             self.handle_tag_event()
             time.sleep(1) #Sleep for 1 second to debounce
